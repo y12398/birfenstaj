6f1cc1d5c21ac64c95a407b2b2c2fee669168e4e
y12398 <74231446+y12398@users.noreply.github.com>
2023-07-29 11:20:57 +0300

    Update go_straight.py

--- scr/go_straight.py
+++ scr/go_straight.py
@@ -1,1 +1,23 @@
+#!/usr/bin/env python3
 
+import rospy
+from geometry_msgs.msg import Twist
+
+
+def movement():
+    rospy.init_node("go_straight")
+    pub = rospy.Publisher("cmd_vel",Twist,queue_size=10)
+    speed_message = Twist()
+    speed_message.linear.x = 0.5
+    distance = 1.5
+    change_place = 0
+    t0 = rospy.Time.now().to_sec()
+    
+    while (change_place < distance):
+        pub.publish(speed_message)
+        t1 = rospy.Time.now().to_sec()
+        change_place = speed_message.linear.x * (t1 - t0)
+        
+    speed_message.linear.x = 0.0
+    pub.publish(speed_message)   
+movement()
