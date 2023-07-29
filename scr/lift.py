f91521487c3f0ed47be9ed2cb943736ad2941833
y12398 <74231446+y12398@users.noreply.github.com>
2023-07-29 11:21:28 +0300

    Add files via upload

---
+++ scr/lift.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import rospy
+from applications.srv import lift_command
+
+
+def Up_Down(request):
+    if (request.estimated_floor < 6) and (istek.hedef_kat > 0):
+        answer = "Lifted up".format(request.floor)
+        return answer
+
+def send_answer():
+    rospy.init_node("server_node")
+    rospy.Service("estimated_floor", lift_command, Up_Down)
+    rospy.spin()
+
+send_answer()
+
