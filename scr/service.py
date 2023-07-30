2f359f315f67e5cdb37db7c2b748edd8f629bbf6
y12398 <74231446+y12398@users.noreply.github.com>
2023-07-30 22:23:49 +0300

    Update service.py

--- scr/service.py
+++ scr/service.py
@@ -3,12 +3,11 @@
 import rospy
 from apps.srv import lift_command
 
-
 def request(x):
     rospy.wait_for_service("estimated_floor")
     try:
         service = rospy.ServiceProxy("estimated_floor", lift_command)
-        answer = servis(x)
+        answer = service(x)
         return answer.deleted
         
     except rospy.ServiceException:
